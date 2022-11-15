7f401778b5d29d377942b9d3d5fc9b292aca910e
Anitha Kandasamy <anitha.jcas@gmail.com>
2022-11-15 10:23:31 +0530

    first component with css

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Mar 20th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$235.45</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
