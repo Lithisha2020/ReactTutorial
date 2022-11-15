29c6544b03a5b13e04de99437df51f871b863ba8
Anitha Kandasamy <anitha.jcas@gmail.com>
2022-11-15 13:53:20 +0530

    Child component css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(propsdata) {
   return (
     <div className="expense-item">
-      
+      <ExpenseDate expdate={propsdata.date}/>
       <div className="expense-item__description">
         <h2>{propsdata.title}</h2>
         <div className="expense-item__price">${propsdata.amount}</div>
